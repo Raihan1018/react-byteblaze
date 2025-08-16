b039600f766c1faa6d6ba8499f37cb97313c7635
Raihan <raihanuddinahmed10@gmail.com>
2025-08-16 21:45:28 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h2 className='text-5xl'>App</h2>
+      <button className='btn btn-primary'>Click me</button>
+    </div>
+  )
+}
+
+export default App
